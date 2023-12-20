8451f44311c266b4507fcc922edd5842ace58b09
Mohnish-Sonkusale <153887010+Mohnish-Sonkusale@users.noreply.github.com>
2023-12-20 15:38:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from llm_code import create_vector, QA_retriever
+
+st.title("LLM Project 🤖 ")
+btn = st.button("Create Knowledgebase") # it only have the acess of the developer
+if btn:
+    create_vector()
+
+question = st.text_input("Question: ")
+
+if question:
+    chain = QA_retriever()
+    response = chain(question)
+
+    st.header("Answer")
+    st.write(response["result"])
+
