5074fae4795235264a6d902ef0b6e538dc5fef2b
Mohnish-Sonkusale <153887010+Mohnish-Sonkusale@users.noreply.github.com>
2023-12-25 18:01:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from llm_code import create_vector, QA_retriever
 
 st.title("LLM Project 🤖 ")
-btn = st.button("Create Knowledgebase") # it only have the acess of the developer
+btn = st.button("Create DB and save ") # it only have the acess of the developer
 if btn:
     create_vector()
 
